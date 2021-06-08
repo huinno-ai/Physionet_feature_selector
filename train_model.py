3a3717add90c54a6d53540b08b54f3105a6093ab
sangil-huinno <80017879+sangil-huinno@users.noreply.github.com>
2021-06-08 10:18:48 +0900

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -7,11 +7,11 @@ from team_code import training_code
 
 if __name__ == '__main__':
     # Parse arguments.
- #   if len(sys.argv) != 3:
-  #      raise Exception('Include the data and model folders as arguments, e.g., python train_model.py data model.')
+    if len(sys.argv) != 3:
+        raise Exception('Include the data and model folders as arguments, e.g., python train_model.py data model.')
 
     data_directory = sys.argv[1]
-    #model_directory = sys.argv[2]
+    model_directory = sys.argv[2]
 
     # Run the training code.
     print('Running training code...')
